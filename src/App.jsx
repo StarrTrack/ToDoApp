0619296938cccbde9e34393a5939b580a22a7056
PetrStarr <petr.starr@gmail.com>
2025-11-21 17:21:27 +0300

    TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import TodoInput from "./components/TodoInput";
 import TodoItem from "./components/TodoItem";
+import TodoList from "./components/TodoList";
 
 export default function App() {
   return (
     <div>
       <TodoInput />
       <TodoItem />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import TodoItem from "./TodoItem";
+import styled from "styled-components";
+
+const EmptyState = styled.div`
+  text-align: center;
+  color: #999;
+  padding: 20px;
+`;
+
+export default function TodoList({ todos = [], onDeleteTodo, onToggleTodo }) {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onDelete={onDeleteTodo}
+          onToggle={onToggleTodo}
+        />
+      ))}
+
+      {todos.length === 0 && <EmptyState>Нет задач</EmptyState>}
+    </div>
+  );
+}
